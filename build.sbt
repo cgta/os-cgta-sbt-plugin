e81b6f4d602ce911cf2ed7fa549f390c68bdaf30
Ben Jackman <ben@jackman.biz>
2014-06-25 15:20:30 -0500

    Removing bintray and simplifying this project
    Signed-off-by: Ben Jackman <ben@jackman.biz>

--- build.sbt
+++ build.sbt
@@ -1,4 +1,3 @@
-import bintray.Keys._
 
 sbtPlugin := true
 
@@ -6,31 +5,8 @@ name := "os-cgta-sbt-plugin"
 
 organization := "biz.cgta"
 
-publishMavenStyle := false
-
 scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature")
 
-bintrayPublishSettings
-
-releaseSettings
-
-repository in bintray := "sbt-plugins"
-
-licenses +=("MIT", url("http://opensource.org/licenses/MIT"))
-
-bintrayOrganization in bintray := Some("cgta")
-
-addSbtPlugin("biz.cgta" % "sbt-x-sjs-plugin" % "0.1.0")
-
-//Also change in OsCgtaSbtPlugin.scala Versions.scalaJs
-addSbtPlugin("org.scala-lang.modules.scalajs" % "scalajs-sbt-plugin" % "0.5.0")
-
-addSbtPlugin("me.lessis" % "bintray-sbt" % "0.1.1")
-
-addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0")
-
-addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.3")
-
 Seq[Setting[_]](shellPrompt <<= (thisProjectRef, version) {
-  (id, v) => _ => "otest-build:%s:%s> ".format(id.project, v)
+  (id, v) => _ => "%s:%s> ".format(id.project, v)
 })
--- src/main/scala/cgta/osbt/OsCgtaSbtPlugin.scala
+++ src/main/scala/cgta/osbt/OsCgtaSbtPlugin.scala
@@ -3,53 +3,15 @@ package cgta.osbt
 import sbt._
 import sbt.Keys._
 
-import sbtrelease.{ReleaseStateTransformations, ReleasePlugin, ReleaseStep}
-import scala.scalajs.sbtplugin.env.nodejs.NodeJSEnv
-import scala.scalajs.sbtplugin.ScalaJSPlugin
-import scala.scalajs.sbtplugin.testing.JSClasspathLoader
-import ScalaJSPlugin._
-
-import cgta.sbtxsjs.SbtXSjsPlugin
-import SbtXSjsPlugin.XSjsProjects
-
-import org.sbtidea.SbtIdeaPlugin
-
 
 object OsCgtaSbtPlugin {
   sys.props("scalac.patmat.analysisBudget") = "512"
-
-  object Versions {
-    lazy val scala      = "2.10.2"
-    //    lazy val crossScala = List("2.10.2", "2.10.3", "2.10.4", "2.11.0", "2.11.1")
-    lazy val crossScala = List("2.10.4", "2.11.1")
-
-    //Also change in build.sbt file
-    lazy val scalaJs = "0.5.0"
-  }
-
-  object SbtPlugins {
-    lazy val scalaJs = addSbtPlugin("org.scala-lang.modules.scalajs" % "scalajs-sbt-plugin" % Versions.scalaJs)
-  }
-
-  object CompilerPlugins {
-    lazy val macrosPlugin = addCompilerPlugin("org.scalamacros" %% "paradise" % "2.0.0" cross CrossVersion.full)
-  }
-
-  object Libs {
-    lazy val macrosQuasi      = Seq("org.scalamacros" %% "quasiquotes" % "2.0.0")
-    lazy val sbtTestInterface = Seq("org.scala-sbt" % "test-interface" % "1.0")
-    lazy val scalaJsTools     = Seq("org.scala-lang.modules.scalajs" %% "scalajs-tools" % Versions.scalaJs)
-    //    lazy val scalaJsPlugin     = Seq("org.scala-lang.modules.scalajs" %% "scalajs-plugin" % Versions.scalaJs)
-    val scalaReflect = "org.scala-lang" % "scala-reflect"
-  }
 
   object Compiler {
     lazy val settings = Seq[Setting[_]](
       scalacOptions += "-deprecation",
       scalacOptions += "-unchecked",
       scalacOptions += "-feature",
-      // can't use this because of cross platform warnings
-      //    scalacOptions += "-Xfatal-warnings",
       scalacOptions += "-language:implicitConversions",
       scalacOptions += "-language:higherKinds"
     )
@@ -61,45 +23,27 @@ object OsCgtaSbtPlugin {
     })
   }
 
-
-  object License {
-    lazy val mit = licenses +=("MIT", url("http://opensource.org/licenses/MIT"))
-  }
-
-  object Bintray {
-    lazy val settings = bintray.Plugin.bintrayPublishSettings :+
-      ((bintray.Keys.bintrayOrganization in bintray.Keys.bintray) := Some("cgta")) :+
-      License.mit
-
-    def repo(name: String) = bintray.Keys.repository in bintray.Keys.bintray := name
-  }
-
-  object Release {
-    lazy val settings = sbtrelease.ReleasePlugin.releaseSettings
-  }
-
-
   lazy val basicSettings =
-    Seq[Setting[_]](
-      organization := "biz.cgta",
-      scalaVersion := Versions.scala) ++
+    Seq[Setting[_]](organization := "biz.cgta") ++
       Compiler.settings ++
-      Prompt.settings ++
-      Bintray.settings ++
-      Release.settings
+      Prompt.settings
 
-
-  def xprojects(name: String): XSjsProjects = {
-    def getBasePackageName(projectName: String, suffix: String = null) = {
-      val root = "cgta"
-      val name = Option(suffix).fold(projectName)(projectName.split(_)(0))
-      root + "." + name
-    }
-    SbtXSjsPlugin.xprojects(name)
-      .settingsAll(basicSettings: _*)
-      .settingsAll(SbtIdeaPlugin.ideaBasePackage := Some(getBasePackageName(name)))
-      .settingsJvm(SbtIdeaPlugin.ideaBasePackage := Some(getBasePackageName(name, "-jvm")))
-      .settingsSjs(ScalaJSPlugin.scalaJSSettings: _*)
-      .settingsSjs(SbtIdeaPlugin.ideaBasePackage := Some(getBasePackageName(name, "-sjs")))
-  }
 }
+
+
+//
+//  object SbtPlugins {
+//    lazy val scalaJs = addSbtPlugin("org.scala-lang.modules.scalajs" % "scalajs-sbt-plugin" % Versions.scalaJs)
+//  }
+//
+//  object CompilerPlugins {
+//    lazy val macrosPlugin = addCompilerPlugin("org.scalamacros" %% "paradise" % "2.0.0" cross CrossVersion.full)
+//  }
+//
+//  object Libs {
+//    lazy val macrosQuasi      = Seq("org.scalamacros" %% "quasiquotes" % "2.0.0")
+//    lazy val sbtTestInterface = Seq("org.scala-sbt" % "test-interface" % "1.0")
+//    lazy val scalaJsTools     = Seq("org.scala-lang.modules.scalajs" %% "scalajs-tools" % Versions.scalaJs)
+//    //    lazy val scalaJsPlugin     = Seq("org.scala-lang.modules.scalajs" %% "scalajs-plugin" % Versions.scalaJs)
+//    val scalaReflect = "org.scala-lang" % "scala-reflect"
+//  }
